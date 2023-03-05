0deef9a502f49af655abbaa42d64e27eaabded44
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2023-03-05 09:14:34 -0500

    subiendo los cambios

--- application/views/ecommerce/inicio.php
+++ application/views/ecommerce/inicio.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda virtual</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>public/fontawesome/css/fontawesome.css">
-  <link href="<?php echo base_url(); ?>public/fontawesome/css/brands.css" rel="stylesheet">
-  <link href="<?php echo base_url(); ?>public/fontawesome/css/solid.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/fontawesome/css/brands.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/fontawesome/css/solid.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap">
     <link rel="stylesheet" href="https://htmlstream.com/preview/front-v4.2/html/assets/css/vendor.min.css">
     <link rel="stylesheet" href="https://htmlstream.com/preview/front-v4.2/html/assets/css/theme.min.css?v=1.0">
-    <link rel="stylesheet" href="https://htmlstream.com/preview/front-v4.2/html/assets/css/theme.min.css?v=1.0">
+    
 </head>
 <body>
     <!-- ========== HEADER ========== -->
