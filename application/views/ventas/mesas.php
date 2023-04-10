689c5631995bdceaf552a74126464ba9fa3609c1
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2023-04-10 14:57:31 -0500

    subiendo los cambios de las mesas

--- application/views/ventas/mesas.php
+++ application/views/ventas/mesas.php
@@ -57,19 +57,103 @@
             </div>
             <div class="card-body">
               <div class="row">
-                <div class="col-md-12">
-                  
-                </div>
-                <div class="col-md-4 mb-4">
-                  <div class="card card-pricing">
-                    <div class="card-body">
+              <?php for($i = 0; $i < 18; $i++ ){?>
+                <div class="col-md-2">
+                  <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <div class="text-center">
+                      <img
+                      src="https://t4.ftcdn.net/jpg/02/55/00/13/360_F_255001343_tzPQgCcfVmeZoVj0ilPjlse9qA8wPpt8.jpg"
+                      class="img-fluid mt-0"
+                      width="130px;"
+                      >
+                      <p>4 personas</p>
+                      <h6>MESA <?php echo $i+1;?>  <i class="fas fa-circle text-success mx-2"></i></h6>
                     </div>
-                  </div>
-                </div>
+                  </a>
+               </div>
+              <?php } ?>
+              </div>
               </div>
             </div>
           </div>
         </div>
+
+<!-- PEDIDOS -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header bg-success">
+        <h1 class="modal-title fs-5 text-white" id="exampleModalLabel">PEDIDOS A LA MESA</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="">Numero Mesa</label>
+              <input type="text" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <label for="">Meser@</label>
+              <input type="text" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="">Propina</label>
+              <input type="text" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="form-group">
+              <label for="">Descuento</label>
+              <input type="text" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <label>Estado</label>
+              <select
+                class="form-control"
+              >
+              <option value="">Disponible</option>
+              <option value="">Ocupada</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <!--  -->
+        <div class="row mt-3">
+          <div class="col-md-12">
+            <div class="table-responsive ">
+              <table class="table table-striped table-hover">
+                <thead style="background-color: black !important;">
+                  <tr>
+                    <th class="text-white">Codigo</th>
+                    <th class="text-white">Producto</th>
+                    <th class="text-white">Cantidad</th>
+                    <th class="text-white">Valor</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 offset-md-9">
+            <h5>TOTAL $99.999.999</h5>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Guardar</button>
+        <button type="button" class="btn btn-danger">Cerrar mesa</button>
+      </div>
+    </div>
+  </div>
+</div>
   <?php require_once("componentes/footer.php"); ?>
 </div>
   <?php require_once("componentes/scripts.php"); ?>
